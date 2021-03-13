f06ea9c07c7efd9498de34b8b4fb7cc23de9bbe3
Wátila Costa <watilafreire26@gmail.com>
2021-03-13 11:40:37 -0300

    user interface created

--- lib/Game.dart
+++ lib/Game.dart
@@ -6,19 +6,68 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  var _result = "";
+
+  void setResult() {
+    setState(() {
+      _result = 'Ganhou!!!';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Joken Po'),
-        backgroundColor: Colors.deepOrange,
+        title: Text('JokenPo'),
+        backgroundColor: Colors.purple,
       ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(16),
+          padding: EdgeInsets.only(top: 30),
           child: Column(
             children: [
-              Text('Teste'),
+              Text(
+                'Escolha do App',
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40, bottom: 80),
+                child: Image.asset('images/padrao.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 80),
+                child: Text(
+                    _result,
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold
+                    )
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('images/pedra.png', height: 110),
+                  Image.asset('images/papel.png', height: 110),
+                  Image.asset('images/tesoura.png', height: 110),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Text(
+                    'Faça sua Escolha',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 23,
+                        fontWeight: FontWeight.normal
+                    )
+                ),
+              ),
             ],
           ),
         ),
